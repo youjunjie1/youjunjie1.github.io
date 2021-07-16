b8069fe678e7662b90768f957b3fc1c1f95ede26
youjunjie1 <87437008+youjunjie1@users.noreply.github.com>
2021-07-17 01:39:24 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+    remove("small");
 });
 });
