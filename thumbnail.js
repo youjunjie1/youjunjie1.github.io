e6616c8cd6060e366c6ddc85d7ef4520848c11bc
youjunjie1 <87437008+youjunjie1@users.noreply.github.com>
2021-07-20 13:54:04 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.classList.remove("small");
 });
 });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-imagea");
+    targetImage.addEventListener("click", function() {
+    targetImage.classList.remove("small");
+});
+});
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-imageb");
+    targetImage.addEventListener("click", function() {
+    targetImage.classList.remove("small");
+});
+});
